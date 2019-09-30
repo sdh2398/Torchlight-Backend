03b6d58f6d50e88211613174cb13a080dc4bb59b
sdh2398 <ehdgus2398@gmail.com>
2019-09-30 18:41:32 +0900

    [Feature] 패키지 구성 및 Entity 생성

---
+++ src/main/java/org/mashup/torchlight/domain/project/repository/ProjectRepository.java
@@ -0,0 +1,10 @@
+package org.mashup.torchlight.domain.project.repository;
+
+import org.mashup.torchlight.domain.project.entity.Project;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProjectRepository extends JpaRepository<Project, Integer> {
+
+}
